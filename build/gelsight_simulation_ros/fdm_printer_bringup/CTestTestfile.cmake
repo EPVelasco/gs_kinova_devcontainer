1a2e5f686c20eca20fedcde94d4593fe58c697b7
EPVelasco <epvelasco1912@gmail.com>
2025-07-14 12:35:58 +0200

    delete build and devel folders

--- build/gelsight_simulation_ros/fdm_printer_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catkin_ws/src/gelsight_simulation_ros/fdm_printer_bringup
-# Build directory: /home/catkin_ws/build/gelsight_simulation_ros/fdm_printer_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
